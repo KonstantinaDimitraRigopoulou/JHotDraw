f7d1e3c6c5cb807801e32944d146c97b2af5edea
mijak22 <143999959+mijak22@users.noreply.github.com>
2026-01-07 15:48:29 +0100

    JGiven

---
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/bdd/DrawEllipseBDDTest.java
@@ -0,0 +1,25 @@
+package org.jhotdraw.draw.bdd;
+
+/*
+import com.tngtech.jgiven.junit.ScenarioTest;
+import org.junit.Test;
+
+public class DrawEllipseBDDTest
+        extends ScenarioTest<GivenCanvas, WhenDesignerDraws, ThenEllipseIsCreated> {
+
+    @Test
+    public void draw_new_ellipse_on_canvas() {
+
+        given().the_ellipse_tool_is_selected()
+               .and().the_canvas_is_empty();
+
+        when().the_designer_clicks_and_drags_on_the_canvas();
+
+        then().an_ellipse_is_created_on_the_canvas()
+              .and().the_ellipse_matches_the_drag_area();
+    }
+}
+*/
+public class DrawEllipseBDDTest {
+    // JGiven test structure shown above.
+}
